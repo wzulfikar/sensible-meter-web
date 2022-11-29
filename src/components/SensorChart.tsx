589febaba10163b184a000800c69aab31a5550b3
Wildan Zulfikar <hey@wzulfikar.com>
2022-11-29 16:42:43 +0200

    Add rechart

--- src/components/SensorChart.tsx
+++ src/components/SensorChart.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   LineChart,
   Line,
@@ -5,17 +6,30 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-  { name: "Page A", uv: 500, pv: 2400, amt: 2400 },
-  { name: "Page A", uv: 600, pv: 2400, amt: 2400 },
-];
-
 export const SensorChart = () => {
+  const [data, setData] = useState([
+    { name: "", uv: 400, pv: 2400, amt: 2400 },
+    { name: "", uv: 500, pv: 2400, amt: 2400 },
+    { name: "", uv: 600, pv: 2400, amt: 2400 },
+  ]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setData([...data, { name: "", uv: 400, pv: 2400, amt: 2400 }]);
+    }, 1000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <LineChart width={1000} height={400} data={data}>
+    <LineChart
+      width={1000}
+      height={400}
+      data={data}
+      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+    >
       <Line type="monotone" dataKey="uv" stroke="#8884d8" />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey="name" />
